5424a0945feb89ea39796d4dfded661daa3be656
Hauke <haukesa@web.de>
2017-11-19 22:08:49 -0500

    Start of animations

--- rpi_ws281x/python/code/movietest.py
+++ rpi_ws281x/python/code/movietest.py
@@ -1,20 +1,62 @@
 import gizeh as gz
 import moviepy.editor as mpy
-from math import sqrt
-W,H = 78,90 # width, height, in pixels
-duration = 2 # duration of the clip, in seconds
+import easing
+
+W, H = 78, 90  # width, height, in pixels
+left, right = W, 0
+duration = 2  # duration of the clip, in seconds
+fps = 15
+surface = gz.Surface(W, H, bg_color=(0, 0, 0))
+
 
 def make_frame(t):
-    surface = gz.Surface(W, H, bg_color=(0, 0, 0))
-    speed = H * ((t * (duration - t))**3) / 4
+    # reset background
+    background = gz.rectangle(xy=(W / 2, H / 2), lx=W, ly=H, fill=rgb_color_alpha(1, 0, 0, 1))
+    background.draw(surface)
 
-    gradient = gz.ColorGradient(type="radial",
-                                stops_colors=[(0, (1, .8, 0)), (1, (0, 0, 0))], xy1=[0, 0], xy2=[0, 0], xy3=[0, W / 2])
-    circle = gz.circle(r=W / 2, fill=gradient).translate(xy=(0, speed - 15))
-    circle.draw(surface)
-    circle = gz.ellipse(W, H, xy = (W/2,H/2),stroke_width=1, stroke=(1,1,1))
-    circle.draw(surface)
+    point_light_through(t, left, rgb_color_alpha(253, 215, 15, .5))
+    point_light_through(t, right, rgb_color_alpha(253, 215, 15, .5))
+
+    point_light_grow_shrink(t, (15, 15), rgb_color_alpha(253, 215, 15, .5))
+    point_light_grow_shrink(t, (W-15, 15), rgb_color_alpha(253, 215, 15, .5))
+
+    # debugging ellipse
+    ellipse = gz.ellipse(W, H, xy=(W / 2, H / 2), stroke_width=1, stroke=(1, 1, 1))
+    ellipse.draw(surface)
     return surface.get_npimage()
 
+
+def point_light_grow_shrink(t, xy, color):
+    # radius = W * (1 + (t * (duration - t)) ** 2) / 6
+    size = 90
+    if t < .5:
+        radius = easing.easeOutQuart(t, 0, size, duration-.5)
+    else:
+        radius = size - easing.easeInQuart(t, 0, size, duration)
+
+    gradient = gz.ColorGradient(type="radial",
+                                stops_colors=[(0, color), (1, (0, 0, 0, 0))],
+                                xy1=[0, 0],
+                                xy2=[0, 0],
+                                xy3=[0, radius / 2])
+    circle = gz.circle(r=radius / 2, fill=gradient).translate(xy=xy)
+    circle.draw(surface)
+
+
+def point_light_through(t, side, color):
+    y = (-H / 5) + (5 * H / 3) * (t / duration)
+    gradient = gz.ColorGradient(type="radial",
+                                stops_colors=[(0, color), (1, (0, 0, 0, 0))],
+                                xy1=[0, 0],
+                                xy2=[0, 0],
+                                xy3=[0, W / 2])
+    circle = gz.circle(r=W / 2, fill=gradient).translate(xy=(side, y))
+    circle.draw(surface)
+
+
+def rgb_color_alpha(red, green, blue, alpha):
+    return red / 255.0, green / 255.0, blue / 255.0, alpha
+
+
 clip = mpy.VideoClip(make_frame, duration=duration)
-clip.write_gif("circle.gif",fps=15, opt="OptimizePlus", fuzz=10)
+clip.write_gif("circle.gif", fps=fps, opt="OptimizePlus", fuzz=10)
--- rpi_ws281x/python/code/transparent_colors.py
+++ rpi_ws281x/python/code/transparent_colors.py
@@ -4,23 +4,23 @@ which overlap to some extent.
 """
 
 import gizeh as gz
-from numpy import pi # <- 3.14... :)
+from numpy import pi  # <- 3.14... :)
 
-L = 90 # <- dimensions of the final picture
+L = 90  # <- dimensions of the final picture
 
-#1,1,1,0 for transparency  
-surface = gz.Surface(78,90, bg_color=(1,1,1)) # <- white background
+# 1,1,1,0 for transparency
+surface = gz.Surface(78, 90, bg_color=(1, 1, 1))  # <- white background
 
 radius = 50
-centers = [ gz.polar2cart(40, angle) for angle in [0, 2*pi/3, 4*pi/3]]
-colors = [ (1,0,0,.4), # <- Semi-tranparent red (R,G,B, transparency)
-           (0,1,0,.4), # <- Semi-tranparent green
-           (0,0,1,.4)] # <- Semi-tranparent blue
+centers = [gz.polar2cart(40, angle) for angle in [0, 2 * pi / 3, 4 * pi / 3]]
+colors = [(1, 0, 0, .4),  # <- Semi-tranparent red (R,G,B, transparency)
+          (0, 1, 0, .4),  # <- Semi-tranparent green
+          (0, 0, 1, .4)]  # <- Semi-tranparent blue
 
-circles = gz.Group( [ gz.circle(radius, xy=center, fill=color,
-                                stroke_width=3, stroke=(0,0,0)) # black stroke
-                      for center, color in zip(centers, colors)] )
+circles = gz.Group([gz.circle(radius, xy=center, fill=color,
+                              stroke_width=3, stroke=(0, 0, 0))  # black stroke
+                    for center, color in zip(centers, colors)])
 
-circles.translate([L/2,L/2]).draw(surface)
+circles.translate([L / 2, L / 2]).draw(surface)
 
 surface.write_to_png("assets/transparent_colors.png")
