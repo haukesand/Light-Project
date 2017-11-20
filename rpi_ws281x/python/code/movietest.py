c2167423e2fb449f72c0c9230cff98013cfc1bbc
SAH1PLY <fixed-term.hauke.sandhaus@bosch.us.com>
2017-11-20 17:55:51 -0500

    multi strip lights

--- rpi_ws281x/python/code/movietest.py
+++ rpi_ws281x/python/code/movietest.py
@@ -25,14 +25,14 @@ def make_frame(t):
     # point_light_grow_shrink(t, (W-15, 15), rgb_color_alpha(253, 215, 15, .5))
 
     # flank_light_pulse(t, xy1=(0,halfH), xy2=(0,-bottomY), color= rgb_color_alpha(231, 37, 66, .5))
-    # flank_light_pulse(t, xy1=(0, -halfH), xy2=(0, bottomY),
-    #                   color=rgb_color_alpha(231, 37, 66, .5))
-    # flank_light_pulse(t, xy1=(-halfW, 0), xy2=(rightX, 0),
-    #                   color=rgb_color_alpha(231, 37, 66, .5))
-    # flank_light_pulse(t, xy1=(halfW, 0), xy2=(-rightX, 0),
-    #                   color=rgb_color_alpha(231, 37, 66, .5))
+    # flank_light_pulse(t, xy1=(0, -halfH), xy2=(0, bottomY), color=rgb_color_alpha(231, 37, 66, .5))
+    # flank_light_pulse(t, xy1=(-halfW, 0), xy2=(rightX, 0), color=rgb_color_alpha(231, 37, 66, .5))
+    # flank_light_pulse(t, xy1=(halfW, 0), xy2=(-rightX, 0),color=rgb_color_alpha(231, 37, 66, .5))
 
-    strip_light_through(t, math.radians(90), H, rgb_color_alpha(17, 155, 255, .5))
+    multi_strip_light_through(t, angle=math.radians(
+        0), thickness=H / 3, color=rgb_color_alpha(17, 155, 255, .5))
+    # light_rotate_around(t, angle = math.radians(0), thickness = H, direction = 1, color = rgb_color_alpha(17, 155, 255, .5))
+
     # debugging ellipse
     ellipse = gz.ellipse(W, H, xy=(halfW, halfH),
                          stroke_width=1, stroke=(1, 1, 1))
@@ -40,28 +40,42 @@ def make_frame(t):
     return surface.get_npimage()
 
 
+def multi_strip_light_through(t, angle, thickness, color):
+    angle += math.radians(90)  # to start animation from front
+
+    gradient = gz.ColorGradient("linear", ((0, rgb_color_alpha(0.0)), (.5, color), (1, rgb_color_alpha(0.0))),
+                                xy1=(-thickness / 2, 0), xy2=(thickness / 2, 0))
+
+    for x in range(1, 6):
+        center = (gz.polar2cart(
+            (thickness * x + thickness * t / duration) - thickness * 4, angle))
+        rect = gz.rectangle(xy=(halfW, halfH), lx=thickness,
+                            ly=H, fill=gradient).rotate(angle, center=(halfW, halfH)).translate(center)
+        rect.draw(surface)
+
+
 def strip_light_through(t, angle, thickness, color):
-    center = (gz.polar2cart((H*2*t/duration)-H, angle))
+    angle += math.radians(90)  # to start animation from front
+    center = (gz.polar2cart((H * 2 * t / duration) - H, angle))
 
     gradient = gz.ColorGradient("linear", ((0, rgb_color_alpha(0)), (.5, color), (1, rgb_color_alpha(0))),
                                 xy1=(-thickness / 2, 0), xy2=(thickness / 2, 0))
 
-    rect = gz.rectangle(xy=(halfW, halfH), lx=H,
-                        ly=thickness, fill=gradient).rotate(angle, center=(halfW, halfH)).translate(center)
+    rect = gz.rectangle(xy=(halfW, halfH), lx=thickness,
+                        ly=H, fill=gradient).rotate(angle, center=(halfW, halfH)).translate(center)
     rect.draw(surface)
 
 
-def light_rotate_around(t, angle, thickness, color):
-    y = (-H / 5) + (5 * H / 3) * (t / duration)
+def light_rotate_around(t, angle, thickness, direction, color):
 
-    angle = 2 * np.pi * (t / duration)
-    center = W * (0.5 + gz.polar2cart(0.1, angle))
+    strength = easing.easeOutQuart(t, 0.0, 1.0, duration / 2)
+    angle = angle + 2 * math.pi * (t / duration) * direction
 
-    gradient = gz.ColorGradient("linear", ((0, (0, 0, 0, 0)), (.5, color), (1, (0, 0, 0, 0))),
-                                xy1=(0, -thickness / 2), xy2=(0, thickness / 2))
+    gradient = gz.ColorGradient("linear", ((0, rgb_color_alpha(0)), (.5, tuple(i * strength for i in color)), (1, rgb_color_alpha(0))),
+                                xy1=(-thickness / 2, 0), xy2=(thickness / 2, 0))
 
-    rect = gz.rectangle(xy=(halfW, halfH), lx=H,
-                        ly=thickness, fill=gradient).translate(center).rotate(angle, center=(halfW, halfH))
+    rect = gz.rectangle(xy=(halfW, 0), lx=H,
+                        ly=thickness, fill=gradient).rotate(angle, center=(halfW, halfH))
     rect.draw(surface)
 
 
@@ -111,10 +125,12 @@ def point_light_through(t, side, color):
 
 def rgb_color_alpha(*colors):
     if len(colors) == 1:
-        r,g,b,a = 0.0, 0.0, 0.0, 0.0
+        r, g, b, a = colors[0], colors[0], colors[0], colors[0]
     else:
-        r,g,b,a = colors[0] / 255.0, colors[1] / 255.0, colors[2] / 255.0, colors[3]
+        r, g, b, a = colors[0] / 255.0, colors[1] / \
+            255.0, colors[2] / 255.0, colors[3]
     return r, g, b, a
+
 
 clip = mpy.VideoClip(make_frame, duration=duration)
 clip.write_gif("circle.gif", fps=fps, opt="OptimizePlus", fuzz=10)
