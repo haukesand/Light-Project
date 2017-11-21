5e95cd23f024c3d176124cf4a485e21d3b8bc169
SAH1PLY <fixed-term.hauke.sandhaus@bosch.us.com>
2017-11-21 14:51:37 -0500

    receive on raspberrypi

--- rpi_ws281x/python/code/receive.py
+++ rpi_ws281x/python/code/receive.py
@@ -1,22 +1,27 @@
 # Adapted from Abraboxabra 1.0 Piero
 
-import time
 from bluetooth import *
+import drawlight
+import threading
+import time
+# Setup the bluetooth connection
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],
-#                   protocols = [ OBEX_UUID ]
+advertise_service(server_sock, "SampleServer",
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  #                   protocols = [ OBEX_UUID ]
                   )
+
+threading.Thread(target=make_frame).start()
 
 print ("Waiting for connection on RFCOMM channel %d" % port)
 
@@ -24,14 +29,21 @@ client_sock, client_info = server_sock.accept()
 print ("Accepted Bluetooth connection from ", client_info)
 
 while True:
-   try:
+    try:
         data = client_sock.recv(1024)
         print("received command %s" % data)
-        if data == 'openclose':
-                
+        if data.startswith('<') && data.endswith('>'):
+            print("openclose")
+            splitted = data[1:-1].split(',')
+            lightfunction = splitted[1]
+            multi_strip_light_through
 
-   except IOError:
-      client_sock, client_info = server_sock.accept()
-      print("Accepted Bluetooth connection from ", client_info)
-      pass
 
+
+    except IOError:
+        client_sock, client_info = server_sock.accept()
+        print("Accepted Bluetooth connection from ", client_info)
+        pass
+
+client_sock.close()
+server_sock.close()
