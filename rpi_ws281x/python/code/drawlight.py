4655dc6d48defae121053c4d14cb3ec7830e1269
Hauke Sandhaus <haukesa@web.de>
2017-11-28 02:47:14 -0500

    Separated files

--- rpi_ws281x/python/code/drawlight.py
+++ rpi_ws281x/python/code/drawlight.py
@@ -26,7 +26,7 @@ def make_frame(t):
     # point_light_through(t, leftX, rgb_color_alpha(253, 215, 15, .5))
     # point_light_through(t, rightX, rgb_color_alpha(253, 215, 15, .5))
 
-    # point_light_grow_shrink(t, (15, 15), rgb_color_alpha(253, 215, 15, .5))
+    point_light_grow_shrink(t, 140, (15, 30), rgb_color_alpha(253, 215, 15, .5))
     # point_light_grow_shrink(t, (W-15, 15), rgb_color_alpha(253, 215, 15, .5))
 
     # flank_light_pulse(t, xy1=(0,halfH), xy2=(0,-bottomY), color= rgb_color_alpha(231, 37, 66, .5))
@@ -34,7 +34,7 @@ def make_frame(t):
     # flank_light_pulse(t, xy1=(-halfW, 0), xy2=(rightX, 0), color=rgb_color_alpha(231, 37, 66, .5))
     # flank_light_pulse(t, xy1=(halfW, 0), xy2=(-rightX, 0),color=rgb_color_alpha(231, 37, 66, .5))
 
-    multi_strip_light_through(t, angle=math.radians(0), thickness=H / 3, color=rgb_color_alpha(17, 155, 255, .5))
+    # multi_strip_light_through(t, angle=math.radians(0), thickness=H / 3, color=rgb_color_alpha(17, 155, 255, .5))
     # light_rotate_around(t, angle = math.radians(0), thickness = H, direction = 1, color = rgb_color_alpha(17, 155, 255, .5))
 
     # debugging ellipse
@@ -96,9 +96,9 @@ def flank_light_pulse(t, xy1, xy2, color):
     rect.draw(surface)
 
 
-def point_light_grow_shrink(t, xy, color):
+def point_light_grow_shrink(t, size, xy, color):
     # radius = W * (1 + (t * (duration - t)) ** 2) / 6
-    size = 90
+    size = size
     if t < .5:
         radius = easing.easeOutQuart(t, 0, size, duration / 2)
     else:
